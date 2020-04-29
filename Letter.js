fd2216dec6cf63fd9b66a97f93d9d3fadcb9d3c7
Terik Hone <terikhone@gmail.com>
2020-04-28 23:59:17 -0600

    started working on letter.js

---
+++ Letter.js
@@ -0,0 +1,28 @@
+/**
+ * Displays the underlying character or a underscore depending on whether or not the user has guess the letter.
+ * @constructor
+ */
+function Letter(letter) {
+  this.character = letter;
+  this.hasBeenGuessed = false;
+
+  this.returnLetter = function () {
+    console.log("Has Been Guessed: ", this.hasBeenGuessed);
+    
+    if(this.hasBeenGuessed) {
+      console.log("Correct Guess: ", this.character);
+      return this.character;
+    } else {
+      console.log("Incorrect: _");
+      return "_";
+    }
+
+  }
+
+  this.checkLetter = function (character) {
+    if(character === this.character) {
+      this.hasBeenGuessed = true;
+    }
+    this.returnLetter();
+  }
+}
